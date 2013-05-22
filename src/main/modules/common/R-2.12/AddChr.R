a2271d0cb7e5e9939d54aa3a5d670f6011381c29
saren <ari-matti.saren@csc.fi>
2013-05-22 16:37:38 +0300

    AddChr

--- src/main/modules/common/R-2.12/AddChr.R
+++ src/main/modules/common/R-2.12/AddChr.R
@@ -1,23 +1,17 @@
 # Utilities for adding "chr" to chromosome names int GTF and FASTA
 
 
-addChrToGtf <- function(file.name) {
-	
-	base.name <- sub("\\.[[:alnum:]]*$", "", file.name)
-	new.name <-
-	system(paste("awk '{ if ($1 ~ /^[0-9,X,Y,MT,Z,W]/) {print "chr"$0} else {print $0} }'", file.name, ">", new.name))
+addChrToGtf <- function(oldfile.name, newfile.name) {
+	# Add chr to chromosomes (assumed to be any name that is plain number or X,Y,Z or W)
+	# Note: awk selected for robustness in detecting whitespaces.
+	system(paste("awk '{ if ($1 ~ /^[0-9,X,Y,Z,W]/) {print \"chr\"$0} else {print $0} }'", oldfile.name, ">", newfile.name))
 	
 }
 
 
-#isGZipFile <- function(file.name) {
-#	
-#	# get file type with the unix file command
-#	file.type = system(paste("file -Lb --mime", file.name), intern=TRUE)
-#	
-#	if (!is.na(pmatch("application/x-gzip", file.type))) {
-#		return(TRUE);
-#	} else { 
-#		return(FALSE);
-#	}
-#}
+addChrToFasta <- function(oldfile.name, newfile.name) {
+	# Add chr to chromosomes (assumed to be any name that is plain number or X,Y,Z or W)
+	# Note: sed selected for speed.
+	system(paste("sed /^.[0-9,X,Y,Z,W]/s/\">\"/\">chr\"/  <", oldfile.name, ">", newfile.name))
+}
+	
